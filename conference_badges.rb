a396f829d29ac7dfd7308ed7b22a263ab9e9089a
Cory Adams <coadams9@gmail.com>
2018-09-30 20:01:23 +0000

    done

---
+++ conference_badges.rb
@@ -0,0 +1,20 @@
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+  
+end
+
+
+guest = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+counter = 0
+def batch_badge_creator(guest)
+  while counter < guest.length
+  puts "Hello, my name is " + guest[counter] + "."
+  
+  counter += 1
+  end
+  
+  
+end
+
+assign_rooms(name, i)
