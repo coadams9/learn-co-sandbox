d9c286fd3a0a633402842b6d0a374a9e95d8cd3b
Cory Adams <coadams9@gmail.com>
2018-09-30 19:38:55 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,7 @@ guest = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 counter = 0
 def badge_maker(guest)
   while counter < guest.length
-  puts "Hello, my name is #{guest[counter]}."
+  puts "Hello, my name is" + guest[counter] + "."
   
   counter += 1
   end
