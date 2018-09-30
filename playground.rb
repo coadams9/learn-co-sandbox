b574aa06f4b1323c7b7b37d97ec9d6f38ff5abeb
Cory Adams <coadams9@gmail.com>
2018-09-30 18:58:12 +0000

    automatically backed up by learn

---
+++ playground.rb
@@ -0,0 +1,14 @@
+teams = ["Falcons", "Bills", "Panthers", "Dolphins"]
+
+def loop_over_array(teams)
+counter = 0
+
+  while counter < teams.length 
+    puts teams[counter]
+  counter += 1  
+  end
+
+
+end
+
+loop_over_array(teams)
